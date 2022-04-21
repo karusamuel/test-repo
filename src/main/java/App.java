8fa818be15531d22a28f6d241df7df32076b9c5c
karu samuel ngugi <karusamuelngugi@gmail.com>
2022-04-21 11:36:17 +0300

    added input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,11 @@
+import java.io.Console;
+
 public class App {
 
     public static void main(String[] args){
       System.out.println("Hello world");
+        Console console = System.console();
+
+        String name  = console.readLine();
     }
 }
